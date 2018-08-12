ce4c2b2fd7ca30833030a8a5eeb8bb1ad817289b
Redx <liqiangzh@126.com>
2018-08-13 00:14:00 +0800

    富文本添加字体

--- src/scripts/utils/index.js
+++ src/scripts/utils/index.js
@@ -58,7 +58,7 @@ const Utils = {
 	createEditor: (selector) => {
 		const editor = new E(selector)
 		editor.customConfig.showLinkImg = false
-		editor.customConfig.menus = ['bold', 'image', 'link']
+		editor.customConfig.menus = ['bold', 'image', 'link', 'fontSize', 'fontName', 'head']
 		editor.customConfig.customUploadImg = (files, insert) => {
 			if (files && files.length > 0) {
 				let file = files[0];
